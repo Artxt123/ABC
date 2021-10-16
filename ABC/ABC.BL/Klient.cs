24ce2cf359634907251e92d8d7520f23c4b0bb88
Artur Oczkowski <alfaix@o2.pl>
2021-10-16 15:31:45 +0200

    3_5 Testowanie klasy

--- ABC/ABC.BL/Klient.cs
+++ ABC/ABC.BL/Klient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ABC.BL
+﻿namespace ABC.BL
 {
     public class Klient
     {
@@ -31,7 +25,7 @@ namespace ABC.BL
         {
             get
             {
-                return Nazwisko + "," + Imie;
+                return Imie + ", " + Nazwisko;
             }
         }
     }
---
+++ ABC/ABC.BLTest/KientTest.cs
@@ -0,0 +1,26 @@
+﻿using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class KientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arrange (zaaranżuj test)
+            Klient klient = new Klient();
+            klient.Imie = "Artur";
+            klient.Nazwisko = "Oczkowski";
+            string oczekiwana = "Artur, Oczkowski";
+
+            //ACT (działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            //Assert (potwierdź test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+    }
+}
