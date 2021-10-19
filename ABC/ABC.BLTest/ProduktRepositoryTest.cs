13c1d0112ae2d8dd70742029fecbe94d77cb3216
Artur Oczkowski <alfaix@o2.pl>
2021-10-19 22:05:28 +0200

    5_7 Produkt Repozytorium Test

---
+++ ABC/ABC.BLTest/ProduktRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ABC.BL;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ProduktRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzProdukt()
+        {
+            //Arange
+            var produktRepository = new ProduktRepository();
+            var oczekiwana = new Produkt(2)
+            {
+                NazwaProduktu = "Klocki",
+                Opis = "Klocki dla dzieci powyżej 3 lat",
+                AktualnaCena = 119.99M
+            };
+
+            //Act
+            var aktualna = produktRepository.Pobierz(2);
+
+            //Assert
+            Assert.AreEqual(oczekiwana.NazwaProduktu, aktualna.NazwaProduktu);
+            Assert.AreEqual(oczekiwana.Opis, aktualna.Opis);
+            Assert.AreEqual(oczekiwana.AktualnaCena, aktualna.AktualnaCena);
+        }
+    }
+}
+
+
