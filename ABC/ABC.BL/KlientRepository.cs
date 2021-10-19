31f69ef413d610dae613343452a37f0cadeb9f41
Artur Oczkowski <alfaix@o2.pl>
2021-10-19 21:46:40 +0200

    5_5 Klient Repozytorium Test

--- ABC/ABC.BL/KlientRepository.cs
+++ ABC/ABC.BL/KlientRepository.cs
@@ -29,7 +29,7 @@ namespace ABC.BL
                 klient.Nazwisko = "Oczkowski";
             }
 
-            return new Klient();
+            return klient;
         }
 
         /// <summary>
---
+++ ABC/ABC.BLTest/KlientRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ABC.BL;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class KlientRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzKlienta()
+        {
+            //Arange
+            var klientRepository = new KlientRepository();
+            var oczekiwana = new Klient(1)
+            {
+                Email = "arturo@o2.pl",
+                Imie = "Artur",
+                Nazwisko = "Oczkowski"
+            };
+
+            //Act
+            var aktualna = klientRepository.Pobierz(1);
+
+            //Assert
+            //Assert.AreEqual(oczekiwana, aktualna); //to jest zbyt ogólne porównanie, które się nie uda, trzeba porównać bardziej szczegółowo każdą z właściwości obiektu Klient
+            Assert.AreEqual(oczekiwana.KlientId, aktualna.KlientId);
+            Assert.AreEqual(oczekiwana.Email, aktualna.Email);
+            Assert.AreEqual(oczekiwana.Imie, aktualna.Imie);
+            Assert.AreEqual(oczekiwana.Nazwisko, aktualna.Nazwisko);
+        }
+    }
+}
