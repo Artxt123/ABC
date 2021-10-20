357c8019f5562532be796e666232e9668fa98670
Artur Oczkowski <alfaix@o2.pl>
2021-10-20 21:49:16 +0200

    6_10 Budowanie klas o jednym celu

--- ABC/ABC.BL/ZamowienieRepository.cs
+++ ABC/ABC.BL/ZamowienieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ABC.BL
 {
@@ -21,6 +22,54 @@ namespace ABC.BL
                 zamowienie.DataZamowienia = new DateTimeOffset(2021, 10, 19, 22, 17, 00, new TimeSpan(2, 0, 0));
 
             return zamowienie;
+        }
+
+        public WyswietlanieZamowienia PobierzZamowienieDoWyswietlenia (int zamowienieId)
+        {
+            // Tworzymy instancję wyswietlenia zamowienia
+            WyswietlanieZamowienia wyswietlanieZamowienia = new WyswietlanieZamowienia();
+
+            //Tu ma być rzeczywisty kod, który pobierze zamówienie do wyświetlenia o wskazanym ID
+
+            //Tymczasowe dane zakodowane na stałe
+            if (zamowienieId == 10)
+            {
+                wyswietlanieZamowienia.Imie = "Artur";
+                wyswietlanieZamowienia.Nazwisko = "Oczkowski";
+                wyswietlanieZamowienia.DataZamowienia = new DateTimeOffset(2021, 10, 20, 21, 30, 00, new TimeSpan(2, 0, 0));
+                wyswietlanieZamowienia.AdresDostawy = new Adres()
+                {
+                    AdresTyp = 1,
+                    Ulica = "Złotowska",
+                    Miasto = "Złotów",
+                    KodPocztowy = "77-400",
+                    Kraj = "Polska"
+                };
+            }
+            wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista = new List<WyswietlaniePozycjiZamowienia>();
+            //Tu ma być kod, który pobiera elementy zamówienia
+
+            //Tymczasowe dane zakodowane na stałe:
+            if (zamowienieId == 10)
+            {
+                var wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Krzesło",
+                    IloscZamowienia = 4,
+                    CenaZakupu = 119.99M
+                };
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+
+                wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Stół",
+                    IloscZamowienia = 2,
+                    CenaZakupu = 240.00M
+                };
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+            }
+
+            return wyswietlanieZamowienia;
         }
 
         /// <summary>
