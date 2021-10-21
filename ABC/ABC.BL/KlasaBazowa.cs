6fed67f92e20ddcb8ac4bf70c7aff0248e3e182c
Artur Oczkowski <alfaix@o2.pl>
2021-10-21 23:44:17 +0200

    7_6 Budowanie klasy bazowej

---
+++ ABC/ABC.BL/KlasaBazowa.cs
@@ -0,0 +1,30 @@
+﻿namespace ABC.BL
+{
+    //Klasy abstrakcyjnej nie można utworzyć za pomocą słowa new; można ją dziedziczyć
+    public abstract class KlasaBazowa
+    //protected - dostępny dla tej klasy i klas potomnych
+    {
+        public bool JestNowy { get; set; }
+        public bool MaZmiany { get; set; }
+        public StanObiektuOpcje StanObiektu { get; set; }
+        public bool DaneSaPrawidlowe
+        { 
+            get
+            {
+                //Aby sprawdzić prawidłowość danych wywołujemy metodę Zwaliduj. Każda klasa potomna ma swoją metodę zwaliduj
+                return Zwaliduj();
+            } 
+        }
+
+        //Aby klasa potomna mogła nadpisać metodę to musi być ona oznaczona jako virtual lub abstract
+        //Metoda virtual może zostać nadpisana, ale nie musi, bo ma swój własny kod
+        //Metoda abstract nie ma swojego kodu i, aby działała musi zostać nadpisana w klasie potomnej
+        public abstract bool Zwaliduj();
+    }
+
+    public enum StanObiektuOpcje
+    {
+        Aktywne,
+        Usuniete
+    }
+}
