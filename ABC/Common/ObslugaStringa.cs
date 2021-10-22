4903f73a0c9b74aef7c698de37afbda2f09cffd1
Artur Oczkowski <alfaix@o2.pl>
2021-10-22 23:22:41 +0200

    8_6 Metody rozszerzeń

--- ABC/Common/ObslugaStringa.cs
+++ ABC/Common/ObslugaStringa.cs
@@ -2,7 +2,11 @@
 {
     public static class ObslugaStringa //klasa statyczna nie może zostać utworzona
     {
-        public static string WstawSpacje(string zrodlo) //aby wywolać metodę statyczną nie musimy tworzyć instancji klasy (nie musimy tworzyć nowego obiektu, tylko podając nazwę klasy możemy od razu odwołać się do metody statycznej
+        //aby wywolać metodę statyczną nie musimy tworzyć instancji klasy (nie musimy tworzyć nowego obiektu, tylko podając nazwę klasy możemy od razu odwołać się do metody statycznej
+        //METODA ROZSZERZENIA MUSI ZNAJDOWAĆ SIĘ W KLASIE STATYCZNEJ i sama musi być statyczna
+        //Metoda statyczna stając się metodą rozszerzoną nie przestaje być statyczną i można jej używać jak zwykłej metody statycznej
+        //Poniższa metoda rozszerzenia nie musi przyjmować przy wywołaniu tego parametru "string zrodlo"; to już nie jest jako parametr, ale bardziej jako reprezentant klasy, którą rozszerzamy
+        public static string WstawSpacje(this string zrodlo) //za pomocą słowa this przekształcamy metodę statyczną w metodę rozszerzająca dla klasy zdefiniowanej w pierwszym parametrze
         {
             var wynik = string.Empty;
 
