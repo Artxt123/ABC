beeb275b89472afeea0d3f454af7daf909ef6ea7
Artur Oczkowski <alfaix@o2.pl>
2021-10-22 22:52:40 +0200

    8_3 Testowanie komponentu

--- ABC/Common/ObslugaStringa.cs
+++ ABC/Common/ObslugaStringa.cs
@@ -10,12 +10,14 @@
             {
                 foreach (var litera in zrodlo)
                 {
-                    if (char.IsUpper(litera))
+                    if (char.IsUpper(litera)) // doajemy spację przed każdą wielką literą, również na początku stringa
                     {
+                        wynik = wynik.Trim(); //przycinamy wszystkie spacje przed i po wielkiej literze i potem dodajemy swoją przed tą wielką literą
                         wynik += " ";
                     }
                     wynik += litera;
                 }
+                wynik = wynik.Trim(); //usuwa spacje na początku i na końcu stringa
             }
 
             return wynik;
---
+++ ABC/CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb0833e4-bd47-4a28-b90d-d548a2bdad70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
