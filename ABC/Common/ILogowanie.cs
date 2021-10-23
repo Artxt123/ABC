15e846e4d2d7adf45123929ec518fad541d393ca
Artur Oczkowski <alfaix@o2.pl>
2021-10-23 21:13:23 +0200

    9_4 Definiowanie interfejsu

---
+++ ABC/Common/ILogowanie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface ILogowanie
+    {
+        //członkowie interfejsu nie wymagają podania modyfikatora dostępu; są domyślnie public
+        //tylko definiujemy członków, ale ich nie implementujemy (np. nie piszemy kodu dla metody)
+        string Log();
+    }
+}
